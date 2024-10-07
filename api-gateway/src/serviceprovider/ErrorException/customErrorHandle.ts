d076ac0eb21a8b8ef0a4ebeff5f4dde6a7f4a89c
Mohamed Nasser AbdelNabi Mohamed Badr - Etisalat Outsource <O-Mohamed.NAbdelNabi@Etisalat.com>
2024-10-07 12:32:59 +0300

    feat/insurance: add insurance company and policy

--- api-gateway/src/serviceprovider/ErrorException/customErrorHandle.ts
+++ api-gateway/src/serviceprovider/ErrorException/customErrorHandle.ts
@@ -23,7 +23,6 @@ export class CustomErrorHandle {
         //     cause: error
         //   });
         console.log("----------------------------------------------------");
-        console.log( error.response);
 
         console.log(error.response.status);
         
